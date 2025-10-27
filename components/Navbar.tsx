193c1d0e25c1f4feea6ea5314f045065c20b6ef2
AlfitoDW <alfitoarts@gmail.com>
2025-10-27 17:46:12 +0700

    update sementara untuk page.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 export default function Navbar(){
   return (
-    <nav className="bg-white shadow-sm sticky top-0 z-50">
+    <nav className="bg-white shadow-sm  ">
         <div className="container mx-auto flex justify-between items-center px-4 py-3">
             <Link href= "/" className="text-xl font-bold text-blue-600">
                 News<span className="text-gray-800">Aggregator</span>
             </Link>
             <div className="space-x-4 text-sm font-medium">
-                <Link href= "/" className="hover:text-blue-600">
+                <Link href= "/" className=" text-gray-950  hover:text-blue-600">
                     Beranda
                 </Link>
-                <Link href= "/news" className="hover:text-blue-600">
+                <Link href= "/news" className="text-gray-950 hover:text-blue-600">
                     Berita
                 </Link>
-                <Link href= "/about" className="hover:text-blue-600">
+                <Link href= "/about" className="text-gray-950 hover:text-blue-600">
                     Tentang
                 </Link>
             </div>
